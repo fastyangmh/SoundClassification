be83fd17fc5f498238532d56b3fb08bb85c1d696
YangMH <fastyangmh@gmail.com>
2021-05-06 16:52:48 +0800

    create the digital_filter and pad_waveform function. rewrite the get_transform_from_file function for support the torchvision transform.

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,35 @@
 # import
 from ruamel.yaml import safe_load
-from torchaudio import transforms
+import torchaudio
+import torchvision
 import torch.nn as nn
+import numpy as np
+import torch.nn.functional as F
+from torchaudio.functional import lowpass_biquad, highpass_biquad
 
 # def
+
+
+def digital_filter(waveform, filter_type, sample_rate, cutoff_freq):
+    if filter_type == 'bandpass':
+        waveform = lowpass_biquad(
+            waveform=waveform, sample_rate=sample_rate, cutoff_freq=max(cutoff_freq),Q=1)
+        waveform = highpass_biquad(
+            waveform=waveform, sample_rate=sample_rate, cutoff_freq=min(cutoff_freq),Q=1)
+    elif filter_type == 'lowpass':
+        waveform = lowpass_biquad(
+            waveform=waveform, sample_rate=sample_rate, cutoff_freq=max(cutoff_freq),Q=1)
+    elif filter_type == 'highpass':
+        waveform = highpass_biquad(
+            waveform=waveform, sample_rate=sample_rate, cutoff_freq=min(cutoff_freq),Q=1)
+    return waveform
+
+
+def pad_waveform(waveform, max_waveform_length):
+    diff = max_waveform_length-len(waveform)
+    pad = (int(np.ceil(diff/2)), int(np.floor(diff/2)))
+    waveform = F.pad(input=waveform, pad=pad)
+    return waveform
 
 
 def load_yaml(file_path):
@@ -16,16 +42,24 @@ def get_transform_from_file(file_path):
     transform_dict = {}
     transform_config = load_yaml(file_path=file_path)
     for stage in transform_config.keys():
-        transform_dict[stage] = []
-        for name, value in transform_config[stage].items():
-            if value is None:
-                transform_dict[stage].append(
-                    eval('transforms.{}()'.format(name)))
-            else:
-                if type(value) is dict:
-                    value = ('{},'*len(value)).format(*
-                                                      ['{}={}'.format(a, b) for a, b in value.items()])
-                transform_dict[stage].append(
-                    eval('transforms.{}({})'.format(name, value)))
-        transform_dict[stage] = nn.Sequential(*transform_dict[stage])
+        transform_dict[stage] = {}
+        for transform_type in transform_config[stage].keys():
+            temp = []
+            for name, value in transform_config[stage][transform_type].items():
+                if transform_type not in ['audio', 'vision']:
+                    assert False, 'please check the transform config.'
+                module_name = 'torchaudio.transforms' if transform_type == 'audio' else 'torchvision.transforms'
+                if value is None:
+                    temp.append(eval('{}.{}()'.format(module_name, name)))
+                else:
+                    if type(value) is dict:
+                        value = ('{},'*len(value)).format(*
+                                                          ['{}={}'.format(a, b) for a, b in value.items()])
+                    temp.append(
+                        eval('{}.{}({})'.format(module_name, name, value)))
+            if transform_type == 'audio':
+                transform_dict[stage][transform_type] = nn.Sequential(*temp)
+            elif transform_type == 'vision':
+                transform_dict[stage][transform_type] = torchvision.transforms.Compose(
+                    temp)
     return transform_dict
